3bf4d87b48c9b39a68071f1f5ff1bbfa1359c3f1
MateuszGdula <48654087+MateuszGdula@users.noreply.github.com>
2019-03-21 17:53:24 +0100

    Slider type setter

--- src/Library/auto_sliding_setter.js
+++ src/Library/auto_sliding_setter.js
@@ -4,7 +4,7 @@ setAutoSliding(){
 	if(!this.configs.autoSlide) return;
 
 	this.sliderInterval = setInterval(() => this.nextSlideRange.click(), this.configs.autoSlideIntervals * 1000);
-
+ 
 	this.slider.addEventListener("mouseover", () => clearInterval(this.sliderInterval));
 	this.slider.addEventListener("mouseenter", () => clearInterval(this.sliderInterval));
 	this.slider.addEventListener("mouseleave", () => this.sliderInterval = setInterval(() =>
